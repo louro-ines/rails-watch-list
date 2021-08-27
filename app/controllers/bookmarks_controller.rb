cd7b15d585ce1bcb310b0054cf8897e65d6c6d49
Ines Louro <inespazlouro@gmail.com>
2021-08-27 12:20:15 +0100

    first commit

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -19,9 +19,3 @@ class BookmarksController < ApplicationController
     params.require(:bookmark).permit(:comment, :movie_id)
   end
 end
-
-
-  # def create
-  #   @restaurant = Restaurant.find(params[:restaurant_id])
-  #   @review = Review.new(review_params)
-  #   @review.restaurant = @restaurant
